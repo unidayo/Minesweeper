bb39f7b7b83901477288249922b7e48d097c3b27
Yuki <square.aj.faces@gmail.com>
2016-04-27 22:41:03 +0900

    Show Field

--- Minesweeper/MinesweeperView/Form1.cs
+++ Minesweeper/MinesweeperView/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Minesweeper;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,68 @@ namespace MinesweeperView
 {
     public partial class Form1 : Form
     {
+        private Field _field;
+
         public Form1()
         {
             InitializeComponent();
+            initializeField();
+        }
+
+        private void initializeField()
+        {
+            const int ROW_CNT = 5;
+            const int COL_CNT = 10;
+            _field = new Field(ROW_CNT, COL_CNT);
+
+            const int HEIGHT = 40;
+            const int WIDTH = 40;
+
+            const int LOC_X_DEFAULT = 10;
+            int locX = LOC_X_DEFAULT;
+            int locY = 10;
+            int ind = 0;
+
+
+            int cellCnt = ROW_CNT * COL_CNT;
+            for (int i = 0; i < cellCnt; i++)
+            {
+                // ボタン生成
+                var bt = new Button();
+                bt.Location = new Point(locX, locY);
+                bt.Name = "Btn" + ind;
+                bt.Size = new Size(HEIGHT, WIDTH);
+                bt.TabIndex = ind;
+                bt.Text = string.Empty;
+                bt.UseVisualStyleBackColor = true;
+                bt.Margin = new Padding(0);
+                bt.MouseDown += Cell_MouseDown;
+                this.Controls.Add(bt);
+
+                // 位置インクリメント
+                ind++;
+                if (ind % ROW_CNT == 0)
+                {
+                    locX = LOC_X_DEFAULT;
+                    locY += WIDTH;
+                }
+                else
+                {
+                    locX += HEIGHT;
+                }
+            }
+
+            Update(_field);
+        }
+
+        private void Cell_MouseDown(object sender, MouseEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Field field)
+        {
+
         }
     }
 }
